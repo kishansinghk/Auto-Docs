a81c904b59a15d172b7aba48d0301edaba0da720
adarshsinghr852 <adarshsingh852@gmail.com>
2025-04-21 16:27:36 +0530

    check

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -15,7 +15,7 @@ const Home = () => {
               <ul className="flex space-x-8">
                 <li>
                   <a href="#features" className="hover:text-blue-200 transition">
-                    Features
+                    Featuresbdkjhskjhsshkjhs
                   </a>
                 </li>
                 <li>
